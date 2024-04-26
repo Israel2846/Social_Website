775daf3de0aef20e26790f744a22c4c65d7c74e3
Israel Colín <israaacolin@gmail.com>
2024-04-26 11:19:28 -0600

    Ligando cuenta con red social

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -142,6 +142,7 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.facebook.FacebookOAuth2',
     'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
 ]
 
 SOCIAL_AUTH_FACEBOOK_KEY = '1476099469668830'
@@ -150,3 +151,19 @@ SOCIAL_AUTH_FACEBBOK_SCOPE = ['email']
 
 SOCIAL_AUTH_TWITTER_KEY = '5fyznI5MIoxCXI6r0voMAGj60'
 SOCIAL_AUTH_TWITTER_SECRET = 'eXaqhLkdYJ2y5eMw87DbWNfn6zCNsvu5nM2iuD7R8sTTC8DAQ3'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '46603385920-eji9ev5p88ae8bdoj5977401bjumvsv2.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
+
+SOCIAL_AUTH_PIPELINE = [
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'account.authentication.create_profile', 
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+]
