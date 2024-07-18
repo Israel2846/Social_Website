dab1cf10c385d193c86891f593379f4897595aad
Israel Colín <israaacolin@gmail.com>
2024-07-17 21:16:59 -0600

    Obteniendo imagenes de web

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
-class ImageForm(forms.ModelForm):
+class ImageCreateForm(forms.ModelForm):
 
     class Meta:
         model = Image
@@ -17,7 +20,23 @@ class ImageForm(forms.ModelForm):
         extension = url.rsplit('.', 1)[1].lower()
 
         if extension not in valid_extensions:
-            raise forms.ValidationError('The given URL does not'\
+            raise forms.ValidationError('The given URL does not'
                                         'match valid image extensions.')
 
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # Download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+
+        if commit:
+            image.save()
+
+        return image
