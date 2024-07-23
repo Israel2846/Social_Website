ce6a511511f926d464bcfe1c623c3eb9808dcd48
Israel Colín <israaacolin@gmail.com>
2024-07-23 10:56:35 -0600

    Likes y dislikes

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'images.apps.ImagesConfig',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
@@ -163,7 +164,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'account.authentication.create_profile', 
+    'account.authentication.create_profile',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
